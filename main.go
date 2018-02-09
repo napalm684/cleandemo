c449f29aa753a9a487b7ebf35653ae0f87cafd9e
Shawn Vause <napalm684@users.noreply.github.com>
2018-02-09 14:26:37 -0500

    Vscode settings main and fix to mock library usage

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/napalm684/cleandemo/delivery/http/handlers"
+	episodeDomain "github.com/napalm684/cleandemo/domain/episode"
+	"github.com/napalm684/cleandemo/usecase"
+	repositoryMock "github.com/napalm684/cleandemo/usecase/repository/mocks"
+)
+
+func main() {
+	repository := &repositoryMock.Episode{} //TODO: Temporary for testing
+	repository.On(
+		"GetEpisodeByName",
+		"Kitten",
+	).Return(&episodeDomain.Episode{
+		Name:    "Kitten",
+		Season:  11,
+		Episode: 6,
+	}, nil)
+
+	episodeInteractor := usecase.NewEpisodeInteractor(repository)
+	episodeHandler := handlers.NewEpisodeHandler(*episodeInteractor)
+
+	http.HandleFunc("/episodes/", episodeHandler.GetEpisodeByName)
+	http.ListenAndServe(":8888", nil)
 }
--- usecase/repository/mocks/Episode.go
+++ usecase/repository/mocks/Episode.go
@@ -12,11 +12,7 @@ type Episode struct {
 
 //GetEpisodeByName - Retrieves episode details using given name
 func (episode *Episode) GetEpisodeByName(name string) (*episodeDomain.Episode, error) {
-	episode.Called(name)
+	args := episode.Called(name)
 
-	return &episodeDomain.Episode{
-		Name:    name,
-		Season:  11,
-		Episode: 6,
-	}, nil
+	return args.Get(0).(*episodeDomain.Episode), args.Error(1)
 }
